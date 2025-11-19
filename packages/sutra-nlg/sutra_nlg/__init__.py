c65df99a8f9b9fd19067d91d84fe68531def919b
Nisheeth Ranjan <nisheethranjan@Nisheeths-MacBook-Air.local>
2025-11-19 17:23:00 +0530

    chore: Remove obsolete ML packages from monorepo
    Deleted packages:
    - sutra-embedding-service (replaced by external sutra-embedder)
    - sutra-nlg-service (replaced by external sutraworks-model)
    - sutra-ml-base-service (obsolete)
    - sutra-ml-base (obsolete)
    - sutra-nlg (obsolete)
    
    These services are now provided by external repositories:
    - ghcr.io/nranjan2code/sutra-embedder:v1.0.1
    - ghcr.io/nranjan2code/sutraworks-model:v1.0.0
    
    Part of Phase 2 ML service architecture cleanup.

--- packages/sutra-nlg/sutra_nlg/__init__.py
+++
@@ -1,8 +0,0 @@
-from .realizer import NLGConfig, NLGRealizer, GroundedSentence, GroundingError
-
-__all__ = [
-    "NLGConfig",
-    "NLGRealizer",
-    "GroundedSentence",
-    "GroundingError",
-]
